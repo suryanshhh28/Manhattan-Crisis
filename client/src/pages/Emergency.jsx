fb0640ff7389375dc7669fa7fe21e3a1bb02914d
Suryansh Chaudhary <suryanshchaudhary28@gmail.com>
2023-11-26 12:44:36 +0530

    added hiw in emergency

--- client/src/pages/Emergency.jsx
+++ client/src/pages/Emergency.jsx
@@ -41,7 +41,7 @@ const Emergency = () => {
             focus:ring-indigo-200
             focus:ring-opacity-50
           "
-                placeholder="Joe Bloggs"
+                placeholder="Joe Rogan"
               />
             </label>
             <label className="block mb-6">
@@ -64,6 +64,28 @@ const Emergency = () => {
             focus:ring-opacity-50
           "
                 placeholder="Robbery, Kidnapping, etc."
+              />
+            </label>
+            <label className="block mb-6">
+              <span className="text-gray-700 p-2">Your location</span>
+              <input
+                name="problem"
+                type="text"
+                className="
+            block
+            p-2
+            w-full
+            mt-1
+            border
+            border-gray-300
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+          "
+                placeholder="Queens, Brooklyn, etc."
               />
             </label>
             <label className="block mb-6">
@@ -131,7 +153,45 @@ const Emergency = () => {
           <img src={men} alt="Helping" width={500} />
         </div>
       </div>
-      <div className="">
+      <div className="bg-black text-white flex flex-col text-center px-2 py-10">
+        <div className="xl:flex Xl:flex-row xl:justify-evenly">
+          <h1 className="text-4xl sm:text-6xl font-bold mb-2 xl:text-7xl xl:my-[12.5%]">
+            How does it <span className="text-orange-500">work?</span>
+          </h1>
+          <img
+            className=" hidden xl:block xl:w-[700px] rounded-lg"
+            src="https://images.unsplash.com/photo-1538115081112-32c7d8401807?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="xl:flex Xl:flex-row xl:justify-evenly mt-8">
+          <img
+            className=" hidden xl:block xl:w-[800px] rounded-lg"
+            src="https://images.unsplash.com/photo-1453873531674-2151bcd01707?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <p className="text-sm mt-2 xl:max-w-xl xl:text-md xl:mt-[5%] uppercase font-bold">
+            The rescue website operates as a lifeline, employing a sophisticated
+            working procedure to save lives and address criminal incidents
+            swiftly and effectively. Users in distress can access the platform
+            and share their location, enabling the system to dispatch
+            specialized teams such as police, firefighters, and ambulances to
+            the scene promptly. The website utilizes advanced GPS tracking
+            technology to pinpoint the exact location of individuals in need,
+            ensuring a rapid response to emergencies. In addition to addressing
+            medical emergencies, the platform also serves as a crucial tool in
+            combating crime. It enables users to report incidents like robbery,
+            kidnapping, and murder, facilitating a coordinated effort between
+            law enforcement agencies and emergency response teams. The website's
+            integrated system streamlines communication and information sharing,
+            fostering a comprehensive approach to public safety that goes beyond
+            traditional emergency services. By merging technology and emergency
+            response strategies, the rescue website plays a pivotal role in
+            saving lives and maintaining community safety.
+          </p>
+        </div>
+      </div>
+      <div className="mt-8">
         <h1 className="text-5xl sm:text-7xl xl:text-8xl font-bold mx-auto text-center">
           Our Corporate <span className="text-orange-500">Philanthropy.</span>
         </h1>
